ed947db49c4eeb9b985f6f085d48bb49f7958694
Ali Arslan <ali.arslan@wrld3d.com>
2018-09-19 17:15:02 +0100

    WIP Fix for streaming camera not correctly updating.

--- Common/Scripts/WRLDARStreamingCameraHandler.cs
+++ Common/Scripts/WRLDARStreamingCameraHandler.cs
@@ -10,9 +10,6 @@ public class WRLDARStreamingCameraHandler : MonoBehaviour
     [SerializeField]
     private MeshRenderer m_streamingMask;
 
-    [SerializeField]
-    private float m_minDistance = 700f;
-
     private void Start()
     {
         m_streamingCamera = GetComponent<Camera> ();
@@ -21,50 +18,15 @@ public class WRLDARStreamingCameraHandler : MonoBehaviour
 
     public void UpdateStreamingCamera()
     {
-        m_streamingCamera.transform.position = m_streamingMask.transform.position;
-
-        float maxDistance = GetCameraDistanceRelativeToBounds (m_streamingMask.bounds, m_streamingCamera);
-
-        if (maxDistance < m_minDistance) 
-        {
-            //Never get too close to origin.
-            maxDistance = m_minDistance;
-        }
-
-        m_streamingCamera.transform.position -= m_streamingCamera.transform.forward * maxDistance;
-        m_streamingCamera.farClipPlane = maxDistance * 1.1f; //Stream a bit further than max distance.
-        m_streamingCamera.nearClipPlane = maxDistance * 0.1f;
-    }
-
-    private float GetCameraDistanceRelativeToBounds(Bounds bounds, Camera cam)
-    {
-        // project a ray from each of the bounding coords to each of the frustum planes
-        float maxDistance = 0;
-        Plane[] frustumPlanes = GeometryUtility.CalculateFrustumPlanes(cam);
-        Plane[] frustumPlanesToUse =
-        {
-            // [0] = Left, [1] = Right, [2] = Down,
-            // [3] = Up,   [4] = Near,  [5] = Far
-            frustumPlanes[0], frustumPlanes[1],
-            frustumPlanes[2], frustumPlanes[3]
-        };
-
-        foreach (Vector3 v in bounds.GetVertices(true))
-        {
-            Ray ray = new Ray(v, cam.transform.forward);
-            foreach (Plane p in frustumPlanesToUse)
-            {
-                float enter;
-                if (p.Raycast(ray, out enter))
-                {
-                    if (enter > 0 && enter > maxDistance)
-                    {
-                        maxDistance = enter;
-                    }
-                }
-            }
-        }
-
-        return maxDistance;
+		Bounds bounds = m_streamingMask.bounds;
+		var cameraDistance = bounds.extents.x / Mathf.Tan ((m_streamingCamera.fieldOfView * Mathf.Deg2Rad) * 0.5f);
+		var scale = m_wrldMap.lossyScale.x;
+		m_streamingCamera.transform.position = bounds.center + Vector3.up * cameraDistance;
+		m_streamingCamera.transform.LookAt(bounds.center, Vector3.forward);
+		m_streamingCamera.aspect = bounds.extents.x / bounds.extents.z;
+		float calculatedNear = cameraDistance - (float)Wrld.Space.EarthConstants.MaxElevation * scale;
+		m_streamingCamera.nearClipPlane = Mathf.Clamp (calculatedNear, 0.1f, cameraDistance);
+		float calculatedFar = cameraDistance + 1000 * scale;
+		m_streamingCamera.farClipPlane = Mathf.Max(calculatedFar, calculatedNear + 0.1f);
     }
 }
